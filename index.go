a560da042d9250c375eaee53cf5d9764e566f3de
abhi9560 <54478454+abhi9560@users.noreply.github.com>
2022-10-10 12:12:39 +0530

    Update index.go

--- index.go
+++ index.go
@@ -3,93 +3,94 @@ package index
 import (
 	"context"
 	"encoding/json"
-	"github.com/aws/aws-lambda-go/lambdacontext"
 	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambdacontext"
 	//"errors"
 	"log"
 	"reflect"
 	"runtime"
-	
-	
 )
-
-
 
 var (
 	// CurrentContext is the last create lambda context object.
 	CurrentContext context.Context
 )
-
+var Ckheader string
 
 func WrapHandler(handler interface{}) interface{} {
 
-	
 	coldStart := true
 
 	return func(ctx context.Context, msg json.RawMessage) (interface{}, error) {
-		
-		
+
 		ctx = context.WithValue(ctx, "cold_start", coldStart)
-		
+
 		UDPConnection()
 		url_path := lambdacontext.FunctionName
+		nvValue := NVCookieMessage(ctx)
+		ndValue := NDCookieMessage(ctx)
+		StartTransactionMessage(ctx, url_path, "", nvValue, ndValue)
 
-		StartTransactionMessage(ctx ,url_path, "")
 		handlerType := reflect.TypeOf(handler)
 		if handlerType.NumIn() == 0 {
 			return reflect.ValueOf(nil), nil
 		}
-	
+
 		messageType := handlerType.In(handlerType.NumIn() - 1)
-		
-		
+
 		var methodName string
-		reqHeader := "" 
-		Sqs  	:= reflect.TypeOf(events.SQSEvent{})
-		Sns  	:= reflect.TypeOf(events.SNSEvent{}) 
-		S3  	:= reflect.TypeOf(events.S3Event{})
-		ApiReq 	:= reflect.TypeOf(events.APIGatewayProxyRequest{})
-		
-		switch messageType{
-			case Sqs:
-				methodName = "index.SQSEventHandler"
-				SQSEventCall(msg)
-			
-			case Sns :
-				
-				methodName = "index.SNSEventHandler"
-				SNSEventCall(msg)
-			case S3 :
-		        	
-				methodName = "index.S3EndpointHandler"
-				S3EventCall(msg)
-			case ApiReq :
-				
-				methodName = "index.ApiEndpointHandler"
-				reqHeader  = ApiGatewayCall(msg,reqHeader)
-			
-			default:
-				methodName = runtime.FuncForPC(reflect.ValueOf(handler).Pointer()).Name()
-				log.Println("methodname costom",methodName)
-		
-			
+		reqHeader := ""
+		Sqs := reflect.TypeOf(events.SQSEvent{})
+		Sns := reflect.TypeOf(events.SNSEvent{})
+		S3 := reflect.TypeOf(events.S3Event{})
+		ApiReq := reflect.TypeOf(events.APIGatewayProxyRequest{})
+
+		switch messageType {
+		case Sqs:
+			methodName = "index.SQSEventHandler"
+			SQSEventCall(msg)
+
+		case Sns:
+
+			methodName = "index.SNSEventHandler"
+			SNSEventCall(msg)
+		case S3:
+
+			methodName = "index.S3EndpointHandler"
+			S3EventCall(msg)
+		case ApiReq:
+
+			methodName = "index.ApiEndpointHandler"
+			reqHeader = ApiGatewayCall(msg, reqHeader)
+
+		default:
+			methodName = runtime.FuncForPC(reflect.ValueOf(handler).Pointer()).Name()
+			//log.Println("methodname costom", methodName)
+
 		}
+		NDCOOKIE_VALUE := NDCookieValue(ctx)
 		statuscode := 200
-		method_entry(ctx,methodName)
-		if reqHeader != ""{
-			SendReqRespHeder(ctx ,reqHeader , "req" ,statuscode)
+		method_entry(ctx, methodName)
+		if reqHeader != "" {
+			SendReqRespHeder(ctx, reqHeader, "req", statuscode)
 		}
+		if CkEnable == true && CkMethodPos == 1 {
+			Ckheader = responsecookies(NDCOOKIE_VALUE)
+		}
+
 		CurrentContext = ctx
-		
-		result, err := callHandler(ctx,msg, handler,messageType)
+
+		result, err := callHandler(ctx, msg, handler, messageType)
 		if err != nil {
 			statuscode = 500
 		}
-	
-		method_exit(ctx,methodName,statuscode)
-		
-		end_business_transaction(ctx,statuscode)
-		
+
+		method_exit(ctx, methodName, statuscode)
+		if CkEnable == true && CkMethodPos > 1 {
+			Ckheader = responsecookies(NDCOOKIE_VALUE)
+		}
+		end_business_transaction(ctx, statuscode)
+
 		CloseUDP()
 		coldStart = false
 		CurrentContext = nil
@@ -97,16 +98,15 @@ func WrapHandler(handler interface{}) interface{} {
 	}
 }
 
-func callHandler(ctx context.Context,msg json.RawMessage, handler interface{},messageType reflect.Type) (interface{}, error) {
-	ev, err := unmarshalEventForHandler(msg,messageType)
+func callHandler(ctx context.Context, msg json.RawMessage, handler interface{}, messageType reflect.Type) (interface{}, error) {
+	ev, err := unmarshalEventForHandler(msg, messageType)
 	if err != nil {
 		return nil, err
 	}
 	handlerType := reflect.TypeOf(handler)
-	
 
 	args := []reflect.Value{}
-	
+
 	if handlerType.NumIn() == 1 {
 		// When there is only one argument, argument is either the event payload, or the context.
 		contextType := reflect.TypeOf((*context.Context)(nil)).Elem()
@@ -120,58 +120,56 @@ func callHandler(ctx context.Context,msg json.RawMessage, handler interface{},me
 	} else if handlerType.NumIn() == 2 {
 		// Or when there are two arguments, context is always first, followed by event payload.
 		args = []reflect.Value{reflect.ValueOf(ctx), ev.Elem()}
-		
-		
+
 	}
 
 	handlerValue := reflect.ValueOf(handler)
-	
-	log.Println("handlerValue   ",handlerValue)
+
+	//log.Println("handlerValue   ", handlerValue)
 	output := handlerValue.Call(args)
-	log.Println("full output",output)
+	//log.Println("full output", output)
 	var response interface{}
-	
+
 	var errResponse error
 
 	if len(output) > 0 {
 		// If there are any output values, the last should always be an error
 		val := output[len(output)-1].Interface()
-		log.Println("val of output",val)
+		//log.Println("val of output", val)
 		if errVal, ok := val.(error); ok {
 			errResponse = errVal
 		}
 	}
-	
+
 	if len(output) > 1 {
 		// If there is more than one output value, the first should be the response payload.
-		response    = output[0].Interface()
-		ApiResp    := reflect.TypeOf(events.APIGatewayProxyRequest{})
-		
-		
-		switch messageType{
-			
-			case ApiResp:
-				respHeader := ""
-				str, err := json.Marshal(response)
-				if err != nil {
-					log.Println("unable to marshal json", err)
-				}
-				respHeader,statuscode := ApiResponseCall(str,respHeader)
-				
-				log.Println("all header value 2",statuscode)
-        			SendReqRespHeder(ctx ,respHeader , "resp" ,statuscode)
+		response = output[0].Interface()
+		ApiResp := reflect.TypeOf(events.APIGatewayProxyRequest{})
+
+		switch messageType {
+
+		case ApiResp:
+			respHeader := ""
+			str, err := json.Marshal(response)
+			if err != nil {
+				log.Println("unable to marshal json", err)
+			}
+			respHeader, responsevent := ApiResponseCall(str, respHeader, Ckheader)
+
+			//log.Println("all header value 2", statuscode)
+			SendReqRespHeder(ctx, respHeader, "resp", responsevent.StatusCode)
+			return responsevent, errResponse
 		}
-		
+
 	}
 
 	return response, errResponse
 }
 
 func unmarshalEventForHandler(ev json.RawMessage, messageType reflect.Type) (reflect.Value, error) {
-	
+
 	newMessage := reflect.New(messageType)
-	json.Unmarshal(ev,newMessage.Interface())
-	
+	json.Unmarshal(ev, newMessage.Interface())
+
 	return newMessage, err
 }
-
